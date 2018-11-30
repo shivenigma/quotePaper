cb207c56cebeafa2b85c51a0d183636e30573749
Vignesh M <vigneshm@agiratech.com>
2018-11-30 10:49:42 +0530

    moved towards random quote wallpaper generation
    Signed-off-by: Vignesh M <vigneshm@agiratech.com>

--- prepare.py
+++ prepare.py
@@ -1,13 +1,36 @@
 import requests
-import csv
+# import re
+from collections import namedtuple
+from random import randint
 from bs4 import BeautifulSoup
+
+Color = namedtuple('Color', ['red', 'green', 'blue'])
+
+
+def random_colors():
+    return Color(randint(0, 255), randint(0, 255), randint(0, 255))
+
+
 page = requests.get('https://www.imdb.com/chart/top')
 soup = BeautifulSoup(page.text, 'html.parser')
 titles = soup.find_all('td', {'class': 'titleColumn'})
-file = csv.writer(open('top.csv', 'w'))
-for title in titles:
-    link = title.find('a')
-    # quotes sample URL, needs to use regex
-    # https://www.imdb.com/title/tt0111161/quotes/
-    file.writerow(['"' + link.contents[0] + '"', 'https://www.imdb.com' + link.get('href').split("?")[0] + 'quotes'])
-print('done');
+selection = randint(1, len(titles))
+link = titles[selection].find('a')
+quote_url = 'https://www.imdb.com' + link.get('href').split("?")[0] + 'quotes'
+title = link.contents[0]
+print('opening page ' + quote_url + 'for movie ' + title);
+page = requests.get(quote_url)
+parsed = BeautifulSoup(page.text, 'html.parser')
+container = parsed.find('div', {'class': 'list'})
+quotes = container.find_all('div', {'class': 'sodavote'})
+quote_selection = randint(1, len(quotes));
+text = quotes[quote_selection].find('div', {'class': 'sodatext'}).text
+# likes = container.find('a', {'class': 'interesting-count-text'}).text
+# if likes:
+#    count = re.findall(r'\d+', likes) or [0]
+# else:
+#    count = [0]
+print(text);
+colors = random_colors()
+for color in colors:
+    print(color);
