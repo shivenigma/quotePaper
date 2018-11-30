32af16503ed20ddf9428241a40aeb0fed99585a1
Vignesh M <vigneshm@agiratech.com>
2018-11-30 10:13:52 +0530

    Finished collecting quotes to csv file
    Signed-off-by: Vignesh M <vigneshm@agiratech.com>

--- prepare.py
+++ prepare.py
@@ -9,5 +9,5 @@ for title in titles:
     link = title.find('a')
     # quotes sample URL, needs to use regex
     # https://www.imdb.com/title/tt0111161/quotes/
-    file.writerow([link.contents[0], 'https://www.imdb.com' + link.get('href').split("?")[0] + 'quotes'])
+    file.writerow(['"' + link.contents[0] + '"', 'https://www.imdb.com' + link.get('href').split("?")[0] + 'quotes'])
 print('done');
--- quotes.py
+++ quotes.py
@@ -1,4 +1,22 @@
 import csv
+import requests
+import re
+from bs4 import BeautifulSoup
 with open('top.csv', newline="") as top:
-    file = csv.reader(top, delimiter=',', quotechar='|')
-    print(len(list(file)))
+    file = csv.reader(top, delimiter=',', quotechar='"')
+    for movie in file:
+        print('starting for' + movie[0])
+        newFile = csv.writer(open('movies/' + movie[0] + '.csv', 'w'))
+        page = requests.get(movie[1])
+        parsed = BeautifulSoup(page.text, 'html.parser')
+        container = parsed.find('div', {'class': 'list'})
+        quotes = container.find_all('div', {'class': 'sodavote'})
+        for quote in quotes:
+            text = quote.find('div', {'class': 'sodatext'}).text
+            likes = container.find('a', {'class': 'interesting-count-text'}).text
+            if likes:
+                count = re.findall(r'\d+', likes) or [0]
+            else:
+                count = [0]
+            newFile.writerow([text, count[0]])
+        print('finished for ' + movie[0])
